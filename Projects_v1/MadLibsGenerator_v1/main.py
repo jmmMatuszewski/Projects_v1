939101d5fbfcbe73eee979d9dd45a6a53d51c2b9
jmmMatuszewski <jmm.matuszewski@gmail.com>
2021-07-12 18:53:36 +0200

    first commit

---
+++ Projects_v1/MadLibsGenerator_v1/main.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('300x300')
+root.title('Mad Libs Generator')
+Label(root, text= "Mad lib gen or something", font = 'arial 20 bold').pack()
+Label(root, text="click any one :", font= 'arial 15 bold').place(x=40, y=80)
